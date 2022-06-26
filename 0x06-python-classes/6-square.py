ab20208dc2c5e64dfcd614ea3b3a2a7c86ccb00a
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-06-26 22:44:32 +0300

    doc file updated

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,26 +1,8 @@
 #!/usr/bin/python3
+"""Square class with tuple, getter, setter and printer method"""
 class Square:
-    """Square Class
-
-    A Square Class
-
-    """
-
+    """Initilizer method which initilizes the tuple postion and the size"""
     def __init__(self, size=0, position=(0, 0)):
-        """__init__
-
-        The __init__ method initializes the size value of the square.
-
-        Attributes:
-            size (:obj:`int`, optional): The size of the square.
-
-        Raises:
-            TypeError: If `size` type is not `int`.
-
-            ValueError: If `size` is less than `0`.
-
-        """
-
         if type(size) is not int:
             raise TypeError('size must be an integer')
 
@@ -42,20 +24,6 @@ class Square:
 
     @size.setter
     def size(self, size):
-        """__init__
-
-        The size setter method update the size value of the square.
-
-        Attributes:
-            size (:obj:`int`): The new size of the square.
-
-        Raises:
-            TypeError: If `size` type is not `int`.
-
-            ValueError: If `size` is less than `0`.
-
-        """
-
         if type(size) is not int:
             raise TypeError('size must be an integer')
 

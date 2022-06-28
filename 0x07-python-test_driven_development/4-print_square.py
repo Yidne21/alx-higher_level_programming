4ab485351d01c31b5ed23dfe1ae452ab461cb447
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-06-28 22:53:30 +0300

    4-print_square.py

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module which prints a square using '#' character"""
+def print_square(size):
+"""Function which prints the square of a given size"""
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    if (type(size) == float) and (size < 0):
+        raise TypeError("size must be an integer")
+
+    for n in range(size):
+        print("{}".format('#' * size))

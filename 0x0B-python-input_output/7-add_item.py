5075ce65686c46ec6574a2e629c25ac519430026
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-07-05 22:55:14 +0300

    Solution for task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,10 +4,10 @@
 
 from os import path
 from sys import argv
-
-
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+
 """importing 7-save_to_json_file and 8-load_from_json_file function to load, add and save an object in to JSON File """
 
 if path.exists('add_item.json'):

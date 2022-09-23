baffadefedaa4d258d78533f104d41ac0f670e8a
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-09-23 22:38:45 +0300

    using requests packages

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""This python script Fetches the URL: https://intranet.hbtn.io/status with `requests` module
-"""
+"""This python script Fetches the URL: https://intranet.hbtn.io/status with `requests` module"""
+
 
 import requests
-
 
 if __name__ == "__main__":
     req = requests.get('https://intranet.hbtn.io/status')

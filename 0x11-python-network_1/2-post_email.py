bbfb860f7089f8da9b0bdd98ae17cb0f00ebae5b
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-09-23 22:10:06 +0300

    fix the pycode style

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""This program Takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8).
-"""
+"""This program Takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)."""
 
 from sys import argv
 from urllib.request import Request, urlopen
@@ -8,9 +7,7 @@ from urllib.parse import urlencode
 
 
 if __name__ == "__main__":
-    data = urlencode({
-                        'email': argv[2]
-                    }).encode('ascii')
+    data = urlencode({'email': argv[2]}).encode('ascii')
     req = Request(argv[1], data)
 
     with urlopen(req) as res:

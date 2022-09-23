a8a9451582e70de2e18a37a07fe845104f60e283
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-09-23 23:02:12 +0300

    fix pycode style'

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""This program Takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)."""
+"""This program Takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8).
+"""
+
 
 from sys import argv
 from urllib.request import Request, urlopen
